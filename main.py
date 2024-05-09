0f1286431042d4cbb690ed8051e1c2c1ae525b20
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-05-10 02:12:58 +0300

    Added user scanner feature

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ INVITES_CHANNEL_ID = os.getenv("INVITES_CHANNEL_ID")
 INVITES_FORUM_CHANNEL_ID = os.getenv("INVITES_FORUM_CHANNEL_ID")
 URL_CHANNEL_ID = os.getenv("URL_CHANNEL_ID")
 URL_FORUM_CHANNEL_ID = os.getenv("URL_FORUM_CHANNEL_ID")
+USER_ID_CHANNEL_ID = os.getenv("USER_ID_CHANNEL_ID")
+USER_FORUM_CHANNEL_ID = os.getenv("USER_FORUM_CHANNEL_ID")
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -41,6 +43,175 @@ async def on_message(message):
         await process_invites(message)
     elif message.channel.id == int(URL_CHANNEL_ID):
         await process_urls(message)
+    elif message.channel.id == int(USER_ID_CHANNEL_ID):
+        await process_user_ids(message)
+
+
+def extract_user_ids(content):
+    user_ids = []
+    for word in content.split():
+        if word.startswith('`') and word.endswith('`'):
+            potential_id = word.strip('`')
+            if potential_id.isdigit():
+                user_ids.append(potential_id)
+    return user_ids
+
+
+async def process_user_ids(message):
+    print(f'Message received in user ID channel: {message.content}')
+    user_ids = extract_user_ids(message.content)
+    for user_id in user_ids:
+        print(f'Extracted user ID: {user_id}')
+        user_info = get_user_info(user_id)
+        if user_info:
+            print(f'User information retrieved for user: {user_id}')
+            existing_post = await check_existing_user_post(user_id)
+            if not existing_post:
+                await create_user_post(message, user_info, user_id)
+            else:
+                await message.channel.send(f'A post for this user already exists: {existing_post.jump_url}')
+        else:
+            print(f'Failed to retrieve user information for user ID: {user_id}')
+
+
+def get_user_info(user_id):
+    url = f'https://discord.com/api/v9/users/{user_id}'
+    headers = {'Authorization': f'Bot {TOKEN}'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+async def check_existing_user_post(user_id):
+    forum_channel = client.get_channel(int(USER_FORUM_CHANNEL_ID))
+    if forum_channel:
+        for thread in forum_channel.threads:
+            thread_message = await thread.fetch_message(thread.id)
+            if f'[USER-' in thread.name and f'**User ID**: {user_id}' in thread_message.content:
+                return thread_message
+    return None
+
+
+async def create_user_post(message, user_info, user_id):
+    forum_channel = client.get_channel(int(USER_FORUM_CHANNEL_ID))
+    if forum_channel:
+        random_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        title = f'[USER-{random_code}] - {message.created_at.strftime("%m/%d/%Y")}'
+        message_content = format_user_info(user_info, user_id, message.author.mention)
+        thread = await forum_channel.create_thread(name=title, content=message_content, auto_archive_duration=60)
+        return thread
+    else:
+        print('Failed to find the user forum channel.')
+        return None
+
+
+def format_user_info(user_info, user_id, author_mention):
+    message = f"**User ID**: {user_id}\n"
+    message += f"**Username**: {user_info['username']}\n"
+    # Global Name
+    global_name = user_info.get('global_name', 'None')
+    message += f"**Global Name**: {global_name}\n"
+    message += f"**Discriminator**: {user_info['discriminator']}\n"
+    # Avatar
+    if 'avatar' in user_info:
+        avatar_url = f"https://cdn.discordapp.com/avatars/{user_id}/{user_info['avatar']}?size=1024"
+        message += f"**Avatar URL**: {avatar_url}\n"
+    else:
+        message += "**Avatar URL**: None\n"
+    # Banner
+    if 'banner' in user_info:
+        banner_url = f"https://cdn.discordapp.com/banners/{user_id}/{user_info['banner']}?size=1024"
+        message += f"**Banner URL**: {banner_url}\n"
+    else:
+        message += "**Banner URL**: None\n"
+    # Avatar Decoration
+    if 'avatar_decoration' in user_info:
+        avatar_decoration_url = f"https://cdn.discordapp.com/avatars/{user_id}/{user_info['avatar_decoration']}?size=1024"
+        message += f"**Avatar Decoration URL**: {avatar_decoration_url}\n"
+    else:
+        message += "**Avatar Decoration URL**: None\n"
+    # Banner Color
+    banner_color = user_info.get('banner_color', 'None')
+    message += f"**Banner Color**: {banner_color}\n"
+    # Accent Color
+    accent_color = user_info.get('accent_color', 'None')
+    message += f"**Accent Color**: {accent_color}\n"
+    # Profile Colors
+    profile_colors = user_info.get('profile_colors', {})
+    message += "**Profile Colors**:\n"
+    for color_name, color_value in profile_colors.items():
+        message += f"- {color_name.replace('_', ' ').title()}: {color_value}\n"
+    # Flags
+    flags = user_info.get('flags', 0)
+    message += f"**Flags**: {flags}\n"
+    message += parse_user_flags(flags)
+    # Public Flags
+    public_flags = user_info.get('public_flags', 0)
+    message += f"**Public Flags**: {public_flags}\n"
+    message += parse_user_flags(public_flags, is_public=True)
+    # Premium Type
+    premium_type = user_info.get('premium_type', 0)
+    message += f"**Premium Type**: {get_premium_type_name(premium_type)}\n"
+    # MFA Enabled
+    mfa_enabled = user_info.get('mfa_enabled', False)
+    message += f"**MFA Enabled**: {mfa_enabled}\n"
+    # System
+    is_system = user_info.get('system', False)
+    message += f"**System**: {is_system}\n"
+    # Verification
+    message += f"**Verified**: {user_info.get('verified', False)}\n"
+    # Email
+    message += f"**Email**: {user_info.get('email', 'None')}\n"
+    # Clan
+    clan = user_info.get('clan', 'None')
+    message += f"**Clan**: {clan}\n"
+    message += f"\n**Report started by**: {author_mention}"
+    # Bot
+    is_bot = user_info.get('bot', False)
+    message += f"**Bot**: {is_bot}\n"
+
+    return message
+
+
+def parse_user_flags(flags, is_public=False):
+    flag_descriptions = ""
+    flag_values = {
+        1 << 0: ("STAFF", "Discord Employee"),
+        1 << 1: ("PARTNER", "Partnered Server Owner"),
+        1 << 2: ("HYPESQUAD", "HypeSquad Events Member"),
+        1 << 3: ("BUG_HUNTER_LEVEL_1", "Bug Hunter Level 1"),
+        1 << 6: ("HYPESQUAD_ONLINE_HOUSE_1", "House Bravery Member"),
+        1 << 7: ("HYPESQUAD_ONLINE_HOUSE_2", "House Brilliance Member"),
+        1 << 8: ("HYPESQUAD_ONLINE_HOUSE_3", "House Balance Member"),
+        1 << 9: ("PREMIUM_EARLY_SUPPORTER", "Early Nitro Supporter"),
+        1 << 10: ("TEAM_PSEUDO_USER", "User is a team"),
+        1 << 14: ("BUG_HUNTER_LEVEL_2", "Bug Hunter Level 2"),
+        1 << 16: ("VERIFIED_BOT", "Verified Bot"),
+        1 << 17: ("VERIFIED_DEVELOPER", "Early Verified Bot Developer"),
+        1 << 18: ("CERTIFIED_MODERATOR", "Moderator Programs Alumni"),
+        1 << 19: ("BOT_HTTP_INTERACTIONS", "Bot uses only HTTP interactions and is shown in the online member list"),
+        1 << 22: ("ACTIVE_DEVELOPER", "User is an Active Developer"),
+    }
+
+    for flag_value, (flag_name, flag_description) in flag_values.items():
+        if is_public and flag_name in ["PREMIUM_EARLY_SUPPORTER", "TEAM_PSEUDO_USER", "VERIFIED_DEVELOPER",
+                                       "CERTIFIED_MODERATOR", "BOT_HTTP_INTERACTIONS"]:
+            continue
+        if flags & flag_value:
+            flag_descriptions += f"- {flag_name}: {flag_description}\n"
+    return flag_descriptions
+
+
+def get_premium_type_name(premium_type):
+    premium_type_names = {
+        0: "None",
+        1: "Nitro Classic",
+        2: "Nitro",
+        3: "Nitro Basic"
+    }
+    return premium_type_names.get(premium_type, "Unknown")
 
 
 async def process_invites(message):
